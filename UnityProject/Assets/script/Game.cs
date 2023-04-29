5ab9a77b1966ad7cfbae4662df681f1ac18aa535
Patrik Sjöberg <voxxar@gmail.com>
2023-04-29 16:40:59 +0200

    Menu scene!

---
+++ UnityProject/Assets/script/Game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StartGame() {
+        print("HEEEJ");
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+}
