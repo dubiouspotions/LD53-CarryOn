f45dc5ee5a55a6cc71733204ffc753f2ef643dff
Jonny Strömberg <jonny.stromberg@gmail.com>
2023-05-02 00:07:48 +0200

    Add background music

---
+++ UnityProject/Assets/Music/DoNotDestroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroy : MonoBehaviour
+{
+  private void Awake()
+  {
+    GameObject[] musicObject = GameObject.FindGameObjectsWithTag("GameMusic");
+    if (musicObject.Length > 1)
+    {
+      Destroy(this.gameObject);
+    }
+    DontDestroyOnLoad(this.gameObject);
+  }
+}
+
+
+
